3a34982b566750bfd5957d6d4e9b4126e3804f13
Sejal bhagat <bhagatsejal08@gmail.com>
2025-12-09 13:32:10 +0530

    Fix PrismaClient import for Render

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,20 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
-import dotenv from "dotenv";
+import pkg from "@prisma/client";
 
-dotenv.config();
+const { PrismaClient } = pkg;
 
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-});
-
-export default prisma;
+export const prisma = new PrismaClient();
